3b0a8e811e1488112bed1bec5915c7a2e5553b25
Caleb Thorsteinson <caleb@thorsteinson.me>
2016-01-17 02:44:10 -0800

    Fixed google strategy usage

--- src/main.js
+++ src/main.js
@@ -58,9 +58,6 @@ passport.use(new GoogleStrategy({
   clientID: GOOGLE_CLIENT_ID,
   clientSecret: GOOGLE_CLIENT_SECRET,
   callbackURL: `http://localhost:${PORT}/auth/google/callback`
-}, (token, tokenSecret, profile, done) => {
-  // This should properly look them up and authenticate them
-  users.deserializeUser(profile.id)
 }))
 
 app.use(passport.initialize())
