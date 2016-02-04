1f63b767b06d76fd14805ff890c10fc87895eb79
Caleb Thorsteinson <caleb@thorsteinson.me>
2016-02-04 10:12:25 +0000

    Users are now represented in a database
    Ditched the redis store, and instead replaced it with a relational
    model that actually allows for retreiving things like authorization levels.
    This all needs to be exposed and made public via a router, and also still
    needs to be tested.

--- src/models/users.js
+++ src/models/users.js
@@ -1,44 +1,116 @@
-const redis = require('redis')
-const bluebird = require('bluebird')
+import {default as Knex} from 'knex'
 
-// Adds async to all redis methods, allowing for usage as a promise
-bluebird.promisifyAll(redis.RedisClient.prototype)
-bluebird.promisifyAll(redis.Multi.prototype)
-
-var redisClient
-
-function userKey ({id}) {
-  return `user:${id}`
+export {
+  init, getById, getByGoogleId, insert, update, doesUserExist, getAll,
+  setAuthority
 }
 
-// This should be called before doing any redis related things
-//
-// Since this is redis specific and not USER specific, maybe it should be
-// moved elsewhere?
-module.exports.initializeRedis = function (redisOptions) {
-  redisClient = redis.createClient(redisOptions)
-  return redisClient
+const DB_PASSWORD = process.env.DB_PASSWORD
+if (!DB_PASSWORD) {
+  console.log(process.env.DB_PASSWORD)
+  throw new Error('DB_PASSWORD env variable not set!')
 }
 
-module.exports.getById = function (id) {
-  return redisClient.getAsync(`user:${id}`)
-    .then(serialUser => JSON.parse(serialUser))
+// Initialze connection to the database
+const knex = Knex({
+  client: 'pg',
+  connection: {
+    host: '127.0.0.1',
+    user: 'postgres',
+    password: DB_PASSWORD,
+    database: 'users'
+  }
+})
+
+const USERS_TABLE = 'users'
+
+// Create table (if it doesn't exist)
+function buildUsers () {
+  return knex.schema.createTableIfNotExists(USERS_TABLE, function (table) {
+    table.increments('id') // Creates auto-incrementing ID
+    table.string('name').notNullable()
+    table.string('authId').unique().notNullable()
+    table.string('email').notNullable()
+    table.text('profile') // This is just the serialized text that we get back from google
+    // This represents how much authority the user has
+    // 0 -> Student
+    // 1 -> TA
+    // 2 -> Teacher
+    // 3 -> Administrator
+    table.int('authLevel').notNullable()
+    table.timestamps()
+  })
 }
 
-module.exports.insert = function (id, user) {
-  return redisClient.setAsync(`user:${id}`, JSON.stringify(user))
-    .then(() => user)
+// Acutally builds the tables if needed
+function init () {
+  return buildUsers()
+    .catch(err => {
+      console.log('Uh oh, encountered db error!')
+      console.log(err)
+      throw err
+    })
 }
 
-module.exports.update = function (user) {
-  redisClient.hmset(userKey(user), user)
-  return user
+// Get's a user by looking up thier database ID (much faster)
+function getById (id) {
+  return knex(USERS_TABLE).where('id', id)
 }
 
-module.exports.doesUserExist = function (id) {
-  return redisClient.existsAsync(`user:${id}`)
+// Get's a user by looking up their GOOGLE id
+function getByGoogleId (id) {
+  return knex(USERS_TABLE).where('authId', id)
 }
 
-module.exports.getAll = function () {
-  // return db.execute('select id,email,displayName,isAdmin from users');
+// Adds a user with the specified GOOGLE id
+// Optionally set their authorization level (defaults to student)
+function insert (id, user, level = 0) {
+  return knex(USERS_TABLE)
+    .insert({
+      authId: id,
+      profile: user.oauthProfile, // deserialize the json
+      name: user.displayName,
+      email: user.email,
+      authLevel: level
+    })
+}
+
+// Updates a user's profile information with new information
+function update (user) {
+  return knex(USERS_TABLE)
+    .where('authId', user.authId)
+    .update({
+      profile: user.oauthProfile,
+      name: user.displayName,
+      email: user.email
+    })
+}
+
+// Returns whether a user exists
+function doesUserExist (id) {
+  return getById(id)
+    .then(user => !!user)
+}
+
+// Gets every single user that is registered
+function getAll () {
+  return knex(USERS_TABLE)
+    .select('*')
+}
+
+// Sets the authority level of a user
+// where authority = {'student', 'teacher', 'ta', 'admin'}
+function setAuthority (id, level) {
+  const levels = {
+    'student': 0,
+    'ta': 1,
+    'teacher': 2,
+    'admin': 3
+  }
+  const rawLevel = levels[level]
+  if (!rawLevel) throw new Error(`Cannot set unknown authority level: ${level}`)
+
+  return knex(USERS_TABLE)
+    .where('id', id)
+    .update('authLevel', rawLevel)
 }
