69129b8f0f565df5f5d0ff516cc1a935089c7a45
Caleb Thorsteinson <caleb@thorsteinson.me>
2016-02-04 10:31:34 +0000

    Modified startup to connect to db

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ const express = require('express')
 const morgan = require('morgan')
 const session = require('express-session')
 const RedisStore = require('connect-redis')(session)
+const redis = require('redis')
 const passport = require('passport')
 const dgram = require('dgram')
 
@@ -21,9 +22,7 @@ const API_ROOT = '/api/v1'
 const REGISTRY_PORT = process.env.REGISTRY_PORT || 8888
 
 // Immediately begin connecting to redis
-const rc = users.initializeRedis({
-  // REDIS_NAME is automatically set by docker
-  // If not found, assume it's running on the localhost
+const rc = redis.createClient({
   host: process.env.REDIS_HOST || '127.0.0.1',
   port: process.env.REDIS_PORT || 6379
 })
@@ -31,59 +30,62 @@ const rc = users.initializeRedis({
 // Set registry to use our default redis store
 registryModel.setStore(rc)
 
-// create the express application
-var app = express()
+// Attempt to connect to the users database
+users.init().then(() => {
+  // create the express application
+  var app = express()
 
-// Setup middleware chain in one place to avoid confusion
-// request logging
-app.use(morgan(process.env.LOG_FORMAT || 'dev'))
-// parse both json and url-encoded body content
-// app.use(bodyParser.json())
-// app.use(bodyParser.urlencoded({extended: false}))
-// support user sessions
-app.use(session({
-  secret: crypto.randomBytes(64).toString('hex'),
-  resave: false,
-  saveUninitialized: false,
-  store: new RedisStore({client: rc})
-}))
+  // Setup middleware chain in one place to avoid confusion
+  // request logging
+  app.use(morgan(process.env.LOG_FORMAT || 'dev'))
+  // parse both json and url-encoded body content
+  // app.use(bodyParser.json())
+  // app.use(bodyParser.urlencoded({extended: false}))
+  // support user sessions
+  app.use(session({
+    secret: crypto.randomBytes(64).toString('hex'),
+    resave: false,
+    saveUninitialized: false,
+    store: new RedisStore({client: rc})
+  }))
 
-// configure passport for user authentication
-passport.serializeUser(auth.serializeUser)
-passport.deserializeUser(auth.deserializeUser)
+  // configure passport for user authentication
+  passport.serializeUser(auth.serializeUser)
+  passport.deserializeUser(auth.deserializeUser)
 
-// Sets up passport to use Google for authentication
-passport.use(localAuth.Strategy())
+  // Sets up passport to use Google for authentication
+  passport.use(localAuth.Strategy())
 
-app.use(passport.initialize())
-app.use(passport.session())
-app.use(localAuth.GoogleRouter())
+  app.use(passport.initialize())
+  app.use(passport.session())
+  app.use(localAuth.GoogleRouter())
 
-// In order to use the main API they must be authenticated
-app.use(API_ROOT, auth.ensureAuth('/signin/google'))
-app.use(API_ROOT, registryController.Router())
+  // In order to use the main API they must be authenticated
+  app.use(API_ROOT, auth.ensureAuth('/signin/google'))
+  app.use(API_ROOT, registryController.Router())
 
-const server = app.listen(PORT, () => {
-  const addr = server.address()
-  if (addr.address === '::') {
-    addr.address = 'localhost'
-  }
-  console.log(`server listening at http://${addr.address}:${addr.port}`)
+  const server = app.listen(PORT, () => {
+    const addr = server.address()
+    if (addr.address === '::') {
+      addr.address = 'localhost'
+    }
+    console.log(`server listening at http://${addr.address}:${addr.port}`)
+  })
+
+  // Startup UDP Registration server
+  const registry = dgram.createSocket('udp4')
+
+  registry.on('error', (err) => {
+    console.log(`Registry error:\n${err.stack}`)
+    registry.close()
+  })
+
+  registry.on('message', registryController.registryHandler)
+
+  registry.on('listening', () => {
+    var address = registry.address()
+    console.log(`Registry listening ${address.address}:${address.port}`)
+  })
+
+  registry.bind(REGISTRY_PORT)
 })
-
-// Startup UDP Registration server
-const registry = dgram.createSocket('udp4')
-
-registry.on('error', (err) => {
-  console.log(`Registry error:\n${err.stack}`)
-  registry.close()
-})
-
-registry.on('message', registryController.registryHandler)
-
-registry.on('listening', () => {
-  var address = registry.address()
-  console.log(`Registry listening ${address.address}:${address.port}`)
-})
-
-registry.bind(REGISTRY_PORT)
